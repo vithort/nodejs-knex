c6951d9ec8b7c34a4ef282780c8251fe615cd9a9
Vithor <vithort@gmail.com>
2021-01-27 10:31:09 -0300

    Curso de Knex #06 - Delete
    Curso de Knex #06 - Delete

--- index.js
+++ index.js
@@ -134,3 +134,16 @@ database
     console.log(err);
   });
 */
+
+// DELETE
+
+database
+  .where({ id: 3 })
+  .delete()
+  .table('games')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
